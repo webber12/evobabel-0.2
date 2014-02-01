a0e220d248865e3c0b5874bf56d1bf33a0b24612
webber <web-ber12@yandex.ru>
2014-02-01 17:30:17 +0200

    format fix

--- assets/snippets/evoBabel/script.js
+++ assets/snippets/evoBabel/script.js
@@ -6,47 +6,47 @@
             'top' : '1px',
             'right' : '1px',
             'background' : '#f1f1f1',
-			'width':'172px',
-			'border':'solid 1px #dddddd',
-			'padding':'1px'
+            'width':'172px',
+            'border':'solid 1px #dddddd',
+            'padding':'1px'
         }
-	});
-	$$('#eB_relations a').set({
+    });
+    $$('#eB_relations a').set({
         styles:{
-			'width':'158px',
-			'display':'block',
-			'height':'20px',
-			'line-height':'20px'
+            'width':'158px',
+            'display':'block',
+            'height':'20px',
+            'line-height':'20px'
         }
-	});
-	$$('#eB_relations img').set({
+    });
+    $$('#eB_relations img').set({
         styles:{
-			'margin-right':'6px'
+            'margin-right':'6px'
         }
-	});
-	$$('.eB_current').set({
+    });
+    $$('.eB_current').set({
         styles:{
-			'width':'158px',
-			'display':'block',
-			'height':'20px',
-			'line-height':'20px',
-			'background':'#fafafa',
-			'padding':'7px 8px 7px 4px',
-			'border-top':'solid 1px #dddddd',
-			'border-bottom':'solid 1px #dddddd'
+            'width':'158px',
+            'display':'block',
+            'height':'20px',
+            'line-height':'20px',
+            'background':'#fafafa',
+            'padding':'7px 8px 7px 4px',
+            'border-top':'solid 1px #dddddd',
+            'border-bottom':'solid 1px #dddddd'
         }
-	});
-	$$('#eB_relations h3').set({
+    });
+    $$('#eB_relations h3').set({
         styles:{
-			'height':'35px',
-			'line-height':'35px',
-			'margin':'0',
-			'text-align':'center',
-			'font-size':'14px',
-			'font-weight':'bold'
+            'height':'35px',
+            'line-height':'35px',
+            'margin':'0',
+            'text-align':'center',
+            'font-size':'14px',
+            'font-weight':'bold'
         }
-	});
-	$j("#eB_relations").parent().parent().hide();
+    });
+    $j("#eB_relations").parent().parent().hide();
     $j("#tabGeneral").prepend($j("#eB_relations"));
 })
 
