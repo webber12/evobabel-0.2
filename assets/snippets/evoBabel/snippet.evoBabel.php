0f8dac4114f2e29280c69d9de314e3ffee292d48
webber <web-ber12@yandex.ru>
2014-02-01 13:18:18 +0200

    fix format

--- assets/snippets/evoBabel/snippet.evoBabel.php
+++ assets/snippets/evoBabel/snippet.evoBabel.php
@@ -2,29 +2,27 @@
 //@author webber (web-ber12@yandex.ru)
 
 //значения по умолчанию на вкладке Свойства - &lang_template_id=id шаблона языка;text;11
-if(!defined('MODX_BASE_PATH')){die('What are you doing? Get out of here!');}
+if (!defined('MODX_BASE_PATH')) {die('What are you doing? Get out of here!');}
 
-$out='';
+$out = '';
 
-if(isset($_REQUEST['id'])&&(int)$_REQUEST['id']!=0){
-	if(isset($params['rel_tv_id'])&&isset($params['lang_template_id'])){	
-		include_once('evoBabel.class.php');
-		$eB=new evoBabel($modx,(int)$_REQUEST['id'],$params);
+if (isset($_REQUEST['id']) && (int)$_REQUEST['id'] != 0) {
+    if (isset($params['rel_tv_id']) && isset($params['lang_template_id'])) {	
+        include_once('evoBabel.class.php');
+        $eB = new evoBabel($modx, (int)$_REQUEST['id'], $params);
 
-		/*****************создаем версии********************/
-		if(isset($_GET['ebabel'])&&(int)$_GET['ebabel']!=0&&isset($_GET['parent'])&&(int)$_GET['parent']!=0){
-			$res=$eB->makeVersion();
-			echo $res;//возвращаем js для переадресации на текущую страницу
-		}
-		/*********************** конец создания версий ****************/
+        /*****************создаем версии********************/
+        if (isset($_GET['ebabel']) && (int)$_GET['ebabel'] != 0 && isset($_GET['parent']) && (int)$_GET['parent'] != 0){
+            $res = $eB->makeVersion();
+            echo $res;//возвращаем js для переадресации на текущую страницу
+        }
+        /*********************** конец создания версий ****************/
 
-	
-		// получаем отформатированный список связей для вывода
-		$out.=$eB->showRelations();
-	}
-	else{
-		$out.='Не задан id TV для хранения языковых связей либо id шаблона языка в настройках сниппета';
-	}
+
+        // получаем отформатированный список связей для вывода
+        $out .= $eB->showRelations();
+    } else {
+        $out .= 'Не задан id TV для хранения языковых связей либо id шаблона языка в настройках сниппета';
+    }
 }
 echo $out;
-?>
