413bf84d11e27f1e8997478aa56564b8fc030f2a
webber <web-ber12@yandex.ru>
2014-11-19 12:44:05 +0200

    фикс имен картинок файлов

--- assets/snippets/evoBabel/config/config.php
+++ assets/snippets/evoBabel/config/config.php
@@ -6,13 +6,13 @@ if (!defined('MODX_BASE_PATH')) {
 }
 
 //активный язык отдельно от списка
-$activeLang = '<div id="curr_lang"><img src="assets/snippets/evoBabel/config/images/flag_[+alias+].png"> <a href="javascript:;">[+name+]</a></div>'; 
+$activeLang = '<div id="curr_lang"><img src="assets/snippets/evoBabel/config/images/lang_[+alias+].png"> <a href="javascript:;">[+name+]</a></div>'; 
 
 //активный язык в списке
-$activeRow = '<div class="active"><img src="assets/snippets/evoBabel/config/images/flag_[+alias+].png"> &nbsp;<a href="[+url+]">[+name+]</a></div>';
+$activeRow = '<div class="active"><img src="assets/snippets/evoBabel/config/images/lang_[+alias+].png"> &nbsp;<a href="[+url+]">[+name+]</a></div>';
 
 //неактивный язык списка
-$unactiveRow = '<div><img src="assets/snippets/evoBabel/config/images/flag_[+alias+].png"> &nbsp;<a href="[+url+]">[+name+]</a></div>';
+$unactiveRow = '<div><img src="assets/snippets/evoBabel/config/images/lang_[+alias+].png"> &nbsp;<a href="[+url+]">[+name+]</a></div>';
 
 //обертка списка языков
 $langOuter = '<div class="other_langs">[+wrapper+]</div>';
